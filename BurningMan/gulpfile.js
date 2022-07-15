30ac734ab0da4015d6852a8953b9540ec5cc7d99
Ilya <74791061+rubenshteyn@users.noreply.github.com>
2022-07-15 16:56:07 +0300

    Delete BurningMan directory

--- BurningMan/gulpfile.js
+++
@@ -1,5 +0,0 @@
-const gulp = require('gulp');
-
-const requireDir = require('require-dir');
-const tasks = requireDir('./tasks');
-
--- BurningMan/src/js/burger.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.header-burger').addEventListener('click', function() {
-    document.querySelector('.header-nav').classList.toggle('open-nav')
-    this.classList.toggle('active-burger')
-})
